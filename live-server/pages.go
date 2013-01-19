7f5f0b5bb87476092df561d51846c9eb67897241
tav <tav@espians.com>
2013-01-19 05:27:58 +0000

    live-server: add html for various response pages.

---
+++ live-server/pages.go
@@ -0,0 +1,36 @@
+// Public Domain (-) 2013 The Espra Authors.
+// See the Espra UNLICENSE file for details.
+
+package main
+
+import (
+	"fmt"
+)
+
+const htmlTop = `<!doctype html>
+<meta charset=utf-8>
+<title>`
+
+const htmlBottom = `</title>
+<link href='//fonts.googleapis.com/css?family=Droid+Sans' rel=stylesheet>
+<style>
+body {
+  font-family: 'Droid Sans', Verdana, sans-serif;
+  font-size: 40px;
+  padding: 10px 7px;
+}
+</style>
+<body>`
+
+var (
+	htmlIndex       = []byte(htmlTop + "Live Server Endpoint" + htmlBottom + "Live Server Endpoint")
+	htmlIndexLength = fmt.Sprintf("%d", len(htmlIndex))
+	html400         = []byte(htmlTop + "400 Bad Request" + htmlBottom + "400 Bad Request")
+	html400Length   = fmt.Sprintf("%d", len(html400))
+	html401         = []byte(htmlTop + "401 Unauthorized" + htmlBottom + "401 Unauthorized")
+	html401Length   = fmt.Sprintf("%d", len(html401))
+	html404         = []byte(htmlTop + "404 Not Found" + htmlBottom + "404 Not Found")
+	html404Length   = fmt.Sprintf("%d", len(html404))
+	html503         = []byte(htmlTop + "503 Service Unavailable" + htmlBottom + "503 Service Unavailable")
+	html503Length   = fmt.Sprintf("%d", len(html503))
+)
