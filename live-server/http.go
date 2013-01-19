218ff0b0915c47c812177d276ea609c2b9a068ad
tav <tav@espians.com>
2013-01-19 05:28:38 +0000

    live-server: extend the common HTTP handling support.

--- live-server/http.go
+++ live-server/http.go
@@ -10,100 +10,182 @@ import (
 	"net"
 	"net/http"
 	"strings"
+	"syscall"
 )
 
 var (
-	behindProxy bool
-	logPrefix   string
+	behindProxy  bool
+	pongLength   = "0"
+	pongResponse = []byte(`handlePing("`)
 )
 
 // Log event types.
 const (
-	HTTP_PING = iota
+	HTTP_BAD_REQUEST = iota
+	HTTP_INTERNAL_ERROR
+	HTTP_MAINTENANCE
+	HTTP_NOT_FOUND
+	HTTP_OK
+	HTTP_PING
 	HTTP_REDIRECT
-	HTTPS_INTERNAL_ERROR
-	HTTPS_MAINTENANCE
-	HTTPS_REDIRECT
-	HTTPS_STATIC
-	HTTPS_UPSTREAM
-	HTTPS_UPSTREAM_ERROR
-	HTTPS_WEBSOCKET
+	HTTP_STATIC
+	HTTP_UNAUTHORIZED
+	HTTP_UPSTREAM_ERROR
+	HTTP_WEBSOCKET
 )
 
-type Redirector struct {
+type Maintainable interface {
+	SetMaintenance(bool)
+}
+
+func handleMaintenance(frontends []Maintainable, initState bool) {
+	if initState {
+		for _, f := range frontends {
+			f.SetMaintenance(true)
+		}
+	}
+	ch := make(chan bool, 1)
+	go func() {
+		for {
+			enable := <-ch
+			for _, f := range frontends {
+				if enable {
+					f.SetMaintenance(true)
+				} else {
+					f.SetMaintenance(false)
+				}
+			}
+		}
+	}()
+	runtime.SignalHandlers[syscall.SIGUSR1] = func() {
+		ch <- true
+	}
+	runtime.SignalHandlers[syscall.SIGUSR2] = func() {
+		ch <- false
+	}
+}
+
+type RedirectServer struct {
 	hsts       string
 	html       []byte
 	htmlLength string
-	pong       []byte
 	url        string
 }
 
-func (redirector *Redirector) ServeHTTP(conn http.ResponseWriter, req *http.Request) {
-
-	if req.URL.Path == "/ping" {
-		headers := conn.Header()
-		headers.Set("Content-Type", "text/plain")
-		headers.Set("Content-Length", "4")
-		conn.WriteHeader(http.StatusOK)
-		conn.Write(redirector.pong)
-		logRequest(HTTP_PING, http.StatusOK, req.Host, req)
+func (s *RedirectServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/ping" {
+		servePong(w, r)
 		return
 	}
-
-	if redirector.hsts != "" {
-		conn.Header().Set("Strict-Transport-Security", redirector.hsts)
+	if s.hsts != "" {
+		w.Header().Set("Strict-Transport-Security", s.hsts)
 	}
-
-	conn.Header().Set("Location", redirector.url)
-	conn.Header().Set("Content-Type", "text/html")
-	conn.Header().Set("Content-Length", redirector.htmlLength)
-	conn.WriteHeader(http.StatusMovedPermanently)
-	conn.Write(redirector.html)
-	logRequest(HTTP_REDIRECT, http.StatusMovedPermanently, req.Host, req)
-
+	w.Header().Set("Location", s.url)
+	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Length", s.htmlLength)
+	w.WriteHeader(http.StatusMovedPermanently)
+	w.Write(s.html)
+	logRequest(HTTP_REDIRECT, http.StatusMovedPermanently, r)
 }
 
-func logRequest(proto, status int, host string, request *http.Request) {
+func logRequest(proto, status int, r *http.Request) {
 	var ip string
-	splitPoint := strings.LastIndex(request.RemoteAddr, ":")
+	splitPoint := strings.LastIndex(r.RemoteAddr, ":")
 	if splitPoint == -1 {
-		ip = request.RemoteAddr
+		ip = r.RemoteAddr
 	} else {
-		ip = request.RemoteAddr[0:splitPoint]
+		ip = r.RemoteAddr[0:splitPoint]
 	}
-	log.InfoData(logPrefix, proto, status, request.Method, host, request.URL,
-		ip, request.UserAgent(), request.Referer())
+	log.InfoData(logPrefix, proto, status, r.Method, r.Host, r.URL,
+		ip, r.UserAgent(), r.Referer())
 }
 
-func serveRedirector(listener net.Listener, redirector *Redirector) {
-	err := http.Serve(listener, redirector)
-	if err != nil {
-		runtime.Error("Couldn't serve HTTP Redirector: %s", err)
-	}
-}
-
-func RunRedirector(host string, port int, url string, hsts int) {
+func runRedirector(host string, port int, url string, hsts int) {
 	if url == "" {
 		return
 	}
-	r := &Redirector{}
+	s := &RedirectServer{}
 	if hsts != 0 {
-		r.hsts = fmt.Sprintf("max-age=%d", hsts)
+		s.hsts = fmt.Sprintf("max-age=%d", hsts)
 	}
-	r.html = []byte(fmt.Sprintf(
+	s.html = []byte(fmt.Sprintf(
 		`Please <a href="%s">click here if your browser doesn't redirect</a> automatically.`,
 		url))
-	r.htmlLength = fmt.Sprintf("%d", len(r.html))
-	r.pong = []byte("PONG")
-	r.url = url
+	s.htmlLength = fmt.Sprintf("%d", len(s.html))
+	s.url = url
+	runHTTP("HTTP Redirector", host, port, s, " -> "+url)
+}
+
+func runHTTP(name string, host string, port int, handler http.Handler, suffix string) {
 	addr := fmt.Sprintf("%s:%d", host, port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		runtime.Error("Cannot listen on %s: %v", addr, err)
 	}
-	go serveRedirector(listener, r)
+	go serveHTTP(name, listener, handler)
 	if host == "" {
 		host = "localhost"
 	}
-	log.Info("HTTP Redirector running on http://%s:%d -> %s", host, port, url)
+	log.Info("%s running on http://%s:%d%s", name, host, port, suffix)
+}
+
+func serveHTTP(name string, listener net.Listener, handler http.Handler) {
+	err := http.Serve(listener, handler)
+	if err != nil {
+		runtime.Error("Couldn't serve %s: %s", name, err)
+	}
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Length", htmlIndexLength)
+	w.WriteHeader(http.StatusOK)
+	w.Write(htmlIndex)
+	logRequest(HTTP_OK, http.StatusOK, r)
+}
+
+func servePong(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/javascript")
+	w.Header().Set("Content-Length", pongLength)
+	w.WriteHeader(http.StatusOK)
+	w.Write(pongResponse)
+	logRequest(HTTP_PING, http.StatusOK, r)
+}
+
+func serve400(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Length", html400Length)
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write(html400)
+	logRequest(HTTP_BAD_REQUEST, http.StatusNotFound, r)
+}
+
+func serve401(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Length", html401Length)
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write(html401)
+	logRequest(HTTP_UNAUTHORIZED, http.StatusUnauthorized, r)
+}
+
+func serve404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Length", html404Length)
+	w.WriteHeader(http.StatusNotFound)
+	w.Write(html404)
+	logRequest(HTTP_NOT_FOUND, http.StatusNotFound, r)
+}
+
+func serve503(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Length", html503Length)
+	w.WriteHeader(http.StatusServiceUnavailable)
+	w.Write(html503)
+	logRequest(HTTP_MAINTENANCE, http.StatusServiceUnavailable, r)
+}
+
+func setupPong(id string) {
+	id += `");`
+	pongResponse = append(pongResponse, id...)
+	pongLength = fmt.Sprintf("%d", len(pongResponse))
 }
