399b67f624211fd59f3e1f10fd7c9b8e9b0cad79
tav <tav@espians.com>
2013-05-09 01:52:26 +0100

    app: add stub backend package.

---
+++ app/espra/backend/backend.go
@@ -0,0 +1,14 @@
+// Public Domain (-) 2011-2012 The Espra Authors.
+// See the Espra UNLICENSE file for details.
+
+package backend
+
+import (
+	"net/http"
+)
+
+func Start(w http.ResponseWriter, r *http.Request) {
+}
+
+func Stop(w http.ResponseWriter, r *http.Request) {
+}
