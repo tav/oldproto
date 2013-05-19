b797424db55e1e91462559e109c09fa2fe090c1d
tav <tav@espians.com>
2013-05-19 05:23:46 +0100

    espra/db: add some more types for logins/sessions/etc.

--- src/espra/db/db.go
+++ src/espra/db/db.go
@@ -32,15 +32,29 @@ const (
 	LinkRefTerm      = "\x05"
 )
 
-// might be good to write a custom Marshaler for this type to speed up JSON Serialisation=
+// Might be good to write a custom Marshaler to speed up
+// JSON Serialisation.
 type Domly []interface{}
 
-type DomlyAttrs map[string]interface{} //string or slice of Domly
+type DomlyAttrs map[string]interface{} // string or Domly
+
+type Content struct {
+	Body       []byte   `datastore:"b,noindex"`
+	Data       []*Field `datastore:"d,noindex"`
+	Head       []byte   `datastore:"h,noindex"`
+	Parents    []string `datastore:"p"`
+	RenderType []string `datastore:"r,noindex"`
+	Version    int      `datastore:"v"`
+}
 
 type Field struct {
 	Key   string
 	Value interface{}
 	Type  uint8
+}
+
+type Index struct {
+	Terms []string `datastore:"t"`
 }
 
 type Item struct {
@@ -54,17 +68,24 @@ type Item struct {
 
 // Author | Publisher
 
-type Index struct {
-	Terms []string `datastore:"t"`
+type Login struct {
+	Confirmed  bool         `datastore:"c"`
+	Email      string       `datastore:"e"`
+	Passphrase []byte       `datastore:"p,noindex"`
+	Scrypt     ScryptParams `datastore:"q,noindex"`
+	Status     string       `datastore:"s"`
+	Username   string       `datastore:"u"`
+	Version    int          `datastore:"v"`
 }
 
-type Content struct {
-	Body       []byte   `datastore:"b,noindex"`
-	Data       []*Field `datastore:"d,noindex"`
-	Head       []byte   `datastore:"h,noindex"`
-	Parents    []string `datastore:"p"`
-	RenderType []string `datastore:"r,noindex"`
-	Version    uint     `datastore:"v"`
+type LoginEmail struct {
+	Email string `datastore:"e"`
+	Login int64  `datastore:"l"`
+}
+
+type LoginUsername struct {
+	Login    int64  `datastore:"l"`
+	Username string `datastore:"u"`
 }
 
 type Namespace struct {
@@ -72,4 +93,28 @@ type Namespace struct {
 
 type Pointer struct {
 	Ref string
+}
+
+type ScryptParams struct {
+	BlockSize       int    `datastore:"b,noindex"`
+	Iterations      int    `datastore:"i,noindex"`
+	Length          int    `datastore:"k,noindex"`
+	Salt            []byte `datastore:"s,noindex"`
+	Parallelisation int    `datastore:"p,noindex"`
+}
+
+type Session struct {
+	Client     string    `datastore:"c,noindex"`
+	Expires    Timestamp `datastore:"e"`
+	Initiated  time.Time `datastore:"i,noindex"`
+	RememberMe bool      `datastore:"r,noindex"`
+	Salt       []byte    `datastore:"s,noindex"`
+}
+
+type User struct {
+	FullName string    `datastore:"f,noindex"`
+	Gender   string    `datastore:"g"`
+	Joined   Timestamp `datastore:"j"`
+	Location string    `datastore:"c"`
+	Version  int       `datastore:"v" json:"-"`
 }
