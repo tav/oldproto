77e56fa616679ba811c1cce5d969f993ea0fe916
Tom Salfield <tom@open.coop>
2013-05-19 05:26:43 +0100

    html2domly: fix syntax and runtime errors.

--- html2domly/html2domly.go
+++ html2domly/html2domly.go
@@ -7,43 +7,50 @@ import (
 	"fmt"
 	"github.com/tav/golly/log"
 	"github.com/tav/golly/optparse"
+	"github.com/tav/golly/runtime"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
 func main() {
+
 	opts := optparse.Parser("Usage: html2domly [options]", "v")
 
-	output_file := opts.String([]string{"-o", "--output"}, "../coffee/templates.coffee", "coffeescript file to compile to", "PATH")
-	template_src_dir := opts.String([]string{"-i", "--input"}, "../etc/domly", "templatate source directory", "PATH")
+	outputFile := opts.String([]string{"-o", "--output"}, "../coffee/templates.coffee", "coffeescript file to compile to", "PATH")
+	templatesSrcDir := opts.String([]string{"-i", "--input"}, "../etc/domly", "templatate source directory", "PATH")
 	os.Args[0] = "html2domly"
 	opts.Parse(os.Args)
 
 	log.AddConsoleLogger()
 
-	var data []byte
-	var err error
-	var pretty_str string
-	dir, err := os.Open(*template_src_dir)
+	var (
+		data      []byte
+		err       error
+		prettyStr string
+		basename  string
+		pretty    bytes.Buffer
+	)
+
+	dir, err := os.Open(*templatesSrcDir)
+	if err != nil {
+		runtime.StandardError(err)
+	}
 	defer dir.Close()
 
+	out, err := os.Create(*outputFile)
 	if err != nil {
-		log.Error("Error: %v", err)
-	}
-	out, err := os.Create(*output_file)
-	if err != nil {
-		log.Error("Error: %v", err)
+		runtime.StandardError(err)
 	}
 	defer out.Close()
 
 	names, err := dir.Readdirnames(0)
 	if err != nil {
-		log.Error("Error: %v", err)
+		runtime.StandardError(err)
 	}
 
 	out.Write([]byte("define 'templates', (exports) ->\n"))
-	var basename string
+
 	for _, name := range names {
 		if strings.HasSuffix(name, ".html") {
 			basename = strings.TrimSuffix(name, ".html")
@@ -52,19 +59,20 @@ func main() {
 			continue
 		}
 
-		template_path := filepath.Join(*template_src_dir, name)
-		data, err = ui.ParseTemplate(template_path)
+		templatePath := filepath.Join(*templatesSrcDir, name)
+		data, err = ui.ParseTemplate(templatePath)
 		if err != nil {
-			log.Error("Error: %v", err)
+			log.StandardError(err)
 		} else {
-			var pretty bytes.Buffer
+
 			err := json.Indent(&pretty, data, ">", "  ")
 			if err != nil {
-				log.Error("Error: %v", err)
+				log.StandardError(err)
 				log.Info("%v", data)
 			} else {
-				pretty_str = pretty.String()
-				log.Info("%v", pretty_str)
+				prettyStr = pretty.String()
+				pretty.Reset()
+				log.Info("%v", prettyStr)
 			}
 			out.Write([]byte(fmt.Sprintf("  exports['%s'] = `%s`\n", basename, data)))
 		}
@@ -72,7 +80,7 @@ func main() {
 	}
 	out.Write([]byte("  return"))
 
-	log.Info("Compiled domly written to: %v", *output_file)
+	log.Info("compiled domly written to: %v", *outputFile)
 
 	log.Wait()
 
