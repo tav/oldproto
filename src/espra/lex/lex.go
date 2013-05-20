2090512df8105ce57fa406102651c847c7979e6d
Tom Salfield <tom@open.coop>
2013-05-20 23:47:17 +0100

    espra/lex: create minimal reusable lexer package from go's template parser code.

--- src/espra/lex/lex.go
+++ src/espra/lex/lex.go
@@ -5,7 +5,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package ui
+package lex
 
 import (
 	"fmt"
@@ -14,190 +14,180 @@ import (
 	"unicode/utf8"
 )
 
-// item represents a token or text string returned from the scanner.
-type Item struct {
-	typ itemType // The type of this item.
-	pos Pos      // The starting position, in bytes, of this item in the input string.
-	val string   // The value of this item.
-}
+type ItemType int
 
-func (i item) String() string {
-	switch {
-	case i.typ == itemEOF:
-		return "EOF"
-	case i.typ == itemError:
-		return i.val
-	case i.typ > itemKeyword:
-		return fmt.Sprintf("<%s>", i.val)
-	case len(i.val) > 10:
-		return fmt.Sprintf("%.10q...", i.val)
-	}
-	return fmt.Sprintf("%q", i.val)
-}
-
-// itemType identifies the type of lex items.
-type itemType int
+const EOF = -1
 
 const (
-	itemError        itemType = iota // error occurred; value is text of error
-	itemBool                         // boolean constant
-	itemChar                         // printable ASCII character; grab bag for comma etc.
-	itemCharConstant                 // character constant
-	itemComplex                      // complex constant (1+2i); imaginary is just a number
-	itemColonEquals                  // colon-equals (':=') introducing a declaration
-	itemEOF
-	itemField      // alphanumeric identifier starting with '.'
-	itemIdentifier // alphanumeric identifier not starting with '.'
-	itemLeftDelim  // left action delimiter
-	itemLeftParen  // '(' inside action
-	itemNumber     // simple number, including imaginary
-	itemPipe       // pipe symbol
-	itemRawString  // raw quoted string (includes quotes)
-	itemRightDelim // right action delimiter
-	itemRightParen // ')' inside action
-	itemSpace      // run of spaces separating arguments
-	itemString     // quoted string (includes quotes)
-	itemText       // plain text
-	itemVariable   // variable starting with '$', such as '$' or  '$1' or '$hello'
-	// Keywords appear after all the rest.
-	itemKeyword  // used only to delimit the keywords
-	itemDot      // the cursor, spelled '.'
-	itemDefine   // define keyword
-	itemElse     // else keyword
-	itemEnd      // end keyword
-	itemIf       // if keyword
-	itemNil      // the untyped nil constant, easiest to treat as a keyword
-	itemRange    // range keyword
-	itemTemplate // template keyword
-	itemWith     // with keyword
+	ItemError ItemType = iota // error occurred; value is text of error
+	ItemEOF
 )
 
-var key = map[string]itemType{
-	".":        itemDot,
-	"define":   itemDefine,
-	"else":     itemElse,
-	"end":      itemEnd,
-	"if":       itemIf,
-	"range":    itemRange,
-	"nil":      itemNil,
-	"template": itemTemplate,
-	"with":     itemWith,
+// Pos represents a byte position in the original input text from which
+// this template was parsed.
+type Pos int
+
+func (p Pos) Position() Pos {
+	return p
 }
 
-const eof = -1
+// Item represents a token or text string returned from the scanner.
+type Item struct {
+	Typ ItemType // The type of this Item.
+	Pos Pos      // The starting position, in bytes, of this Item in the input string.
+	Val string   // The valVCue of this Item.
+}
+
+func (i Item) String() string {
+	switch {
+	case i.Typ == ItemEOF:
+		return "EOF"
+	case i.Typ == ItemError:
+		return i.Val
+	case len(i.Val) > 10:
+		return fmt.Sprintf("%.10q...", i.Val)
+	}
+	return fmt.Sprintf("%q", i.Val)
+}
+
+type IntState [16]int
+
+type StrState [16]string
 
 // stateFn represents the state of the scanner as a function that returns the next state.
-type stateFn func(*lexer) stateFn
+type StateFn func(*Lexer) StateFn
 
-// lexer holds the state of the scanner.
+// Lexer holds the state of the scanner.
 type Lexer struct {
-	name       string    // the name of the input; used only for error reports
-	input      string    // the string being scanned
-	leftDelim  string    // start of action
-	rightDelim string    // end of action
-	state      stateFn   // the next lexing function to enter
-	pos        Pos       // current position in the input
-	start      Pos       // start position of this item
-	width      Pos       // width of last rune read from input
-	lastPos    Pos       // position of most recent item returned by nextItem
-	items      chan item // channel of scanned items
-	parenDepth int       // nesting depth of ( ) exprs
+	Name       string  // the name of the input; used only for error reports
+	Input      string  // the string being scanned
+	StateFn    StateFn // the next lexing function to enter
+	StrState   StrState
+	IntState   IntState
+	Pos        Pos       // current position in the input
+	Start      Pos       // start position of this item
+	Width      Pos       // width of last rune read from input
+	LastPos    Pos       // position of most recent item returned by nextItem
+	Items      chan Item // channel of scanned Items
+	ParenDepth int       // nesting depth of ( ) exprs
 }
 
 // next returns the next rune in the input.
-func (l *lexer) next() rune {
-	if int(l.pos) >= len(l.input) {
-		l.width = 0
-		return eof
+func (l *Lexer) Next() rune {
+	if int(l.Pos) >= len(l.Input) {
+		l.Width = 0
+		return EOF
 	}
-	r, w := utf8.DecodeRuneInString(l.input[l.pos:])
-	l.width = Pos(w)
-	l.pos += l.width
+	r, w := utf8.DecodeRuneInString(l.Input[l.Pos:])
+	l.Width = Pos(w)
+	l.Pos += l.Width
 	return r
 }
 
-// peek returns but does not consume the next rune in the input.
-func (l *lexer) peek() rune {
-	r := l.next()
-	l.backup()
+// Peek returns but does not consume the next rune in the input.
+func (l *Lexer) Peek() rune {
+	r := l.Next()
+	l.Backup()
 	return r
 }
 
-// backup steps back one rune. Can only be called once per call of next.
-func (l *lexer) backup() {
-	l.pos -= l.width
+// Backup steps back one rune. Can only be called once per call of next.
+func (l *Lexer) Backup() {
+	l.Pos -= l.Width
 }
 
-// emit passes an item back to the client.
-func (l *lexer) emit(t itemType) {
-	l.items <- item{t, l.start, l.input[l.start:l.pos]}
-	l.start = l.pos
+// emit passes an Item back to the client.
+func (l *Lexer) Emit(t ItemType) {
+	l.Items <- Item{t, l.Start, l.Input[l.Start:l.Pos]}
+	l.Start = l.Pos
 }
 
 // ignore skips over the pending input before this point.
-func (l *lexer) ignore() {
-	l.start = l.pos
+func (l *Lexer) Ignore() {
+	l.Start = l.Pos
 }
 
 // accept consumes the next rune if it's from the valid set.
-func (l *lexer) accept(valid string) bool {
-	if strings.IndexRune(valid, l.next()) >= 0 {
+func (l *Lexer) Accept(valid string) bool {
+	if strings.IndexRune(valid, l.Next()) >= 0 {
 		return true
 	}
-	l.backup()
+	l.Backup()
 	return false
 }
 
-// acceptRun consumes a run of runes from the valid set.
-func (l *lexer) acceptRun(valid string) {
-	for strings.IndexRune(valid, l.next()) >= 0 {
+// AcceptRun consumes a run of runes from the valid set.
+func (l *Lexer) AcceptRun(valid string) {
+	for strings.IndexRune(valid, l.Next()) >= 0 {
 	}
-	l.backup()
+	l.Backup()
 }
 
 // lineNumber reports which line we're on, based on the position of
-// the previous item returned by nextItem. Doing it this way
+// the previous Item returned by nextItem. Doing it this way
 // means we don't have to worry about peek double counting.
-func (l *lexer) lineNumber() int {
-	return 1 + strings.Count(l.input[:l.lastPos], "\n")
+func (l *Lexer) LineNumber() int {
+	return 1 + strings.Count(l.Input[:l.LastPos], "\n")
 }
 
 // errorf returns an error token and terminates the scan by passing
 // back a nil pointer that will be the next state, terminating l.nextItem.
-func (l *lexer) errorf(format string, args ...interface{}) stateFn {
-	l.items <- item{itemError, l.start, fmt.Sprintf(format, args...)}
+func (l *Lexer) Errorf(format string, args ...interface{}) StateFn {
+	l.Items <- Item{ItemError, l.Start, fmt.Sprintf(format, args...)}
 	return nil
 }
 
-// nextItem returns the next item from the input.
-func (l *lexer) nextItem() item {
-	item := <-l.items
-	l.lastPos = item.pos
+// nextItem returns the next Item from the input.
+func (l *Lexer) NextItem() Item {
+	item := <-l.Items
+	l.LastPos = item.Pos
 	return item
 }
 
-// lex creates a new scanner for the input string.
-func lex(name, input, left, right string) *lexer {
-	if left == "" {
-		left = leftDelim
+func (l *Lexer) ScanNumber() bool {
+	// Optional leading sign.
+	l.Accept("+-")
+	// Is it hex?
+	digits := "0123456789"
+	if l.Accept("0") && l.Accept("xX") {
+		digits = "0123456789abcdefABCDEF"
 	}
-	if right == "" {
-		right = rightDelim
+	l.AcceptRun(digits)
+	if l.Accept(".") {
+		l.AcceptRun(digits)
 	}
-	l := &lexer{
-		name:       name,
-		input:      input,
-		leftDelim:  left,
-		rightDelim: right,
-		items:      make(chan item),
+	if l.Accept("eE") {
+		l.Accept("+-")
+		l.AcceptRun("0123456789")
 	}
-	go l.run()
-	return l
+	// Is it imaginary?
+	l.Accept("i")
+	// Next thing mustn't be alphanumeric.
+	if IsAlphaNumeric(l.Peek()) {
+		l.Next()
+		return false
+	}
+	return true
 }
 
-// run runs the state machine for the lexer.
-func (l *lexer) run() {
-	for l.state = lexText; l.state != nil; {
-		l.state = l.state(l)
+// isSpace reports whether r is a space character.
+func IsSpace(r rune) bool {
+	return r == ' ' || r == '\t'
+}
+
+// isEndOfLine reports whether r is an end-of-line character.
+func IsEndOfLine(r rune) bool {
+	return r == '\r' || r == '\n'
+}
+
+// isAlphaNumeric reports whether r is an alphabetic, digit, or underscore.
+func IsAlphaNumeric(r rune) bool {
+	return r == '_' || unicode.IsLetter(r) || unicode.IsDigit(r)
+}
+
+// run runs the state machine for the Lexer.
+func (l *Lexer) Run(startfn StateFn) {
+	for l.StateFn = startfn; l.StateFn != nil; {
+		l.StateFn = l.StateFn(l)
 	}
 }
