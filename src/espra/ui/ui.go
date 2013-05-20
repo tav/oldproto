67b221a61a6e74c85ac13ceb08ca6211dae504c7
Tom Salfield <tom@open.coop>
2013-05-20 23:50:18 +0100

    espra/ui: initial domly textNode lexer.

--- src/espra/ui/ui.go
+++ src/espra/ui/ui.go
@@ -9,12 +9,26 @@ import (
 	"code.google.com/p/go.net/html/atom"
 	"encoding/json"
 	"espra/db"
+	"espra/lex"
 	"fmt"
 	"os"
-	"text/template/parse"
+	"strings"
 )
 
 var bodyNode = &html.Node{Data: "body", DataAtom: atom.Body, Type: html.ElementNode}
+
+const (
+	leftDelim  = "{{"
+	rightDelim = "}}"
+)
+
+const (
+	ItemText lex.ItemType = 2 + iota
+	ItemVar
+	ItemSpace
+	ItemLeftDelim
+	ItemRightDelim
+)
 
 var replacements = map[string]string{
 	"cellpadding":     "cellPadding",
@@ -36,8 +50,124 @@ func ParseHTML5(filename string) ([]*html.Node, error) {
 	return frag, err
 }
 
+func createLexer(name, input string, startfn lex.StateFn) *lex.Lexer {
+	// conf = map{}  -- add conf map to the lexer to allow lexer configuration
+	l := &lex.Lexer{
+		Name:  name,
+		Input: input,
+		Items: make(chan lex.Item),
+		/* IntState: lex.IntState{
+			StateLeftDelimLen: len(leftDelim),
+		}, */
+	}
+	go l.Run(startfn)
+	return l
+}
+
+func Variable(l *lex.Lexer) lex.StateFn {
+	for {
+		if !lex.IsAlphaNumeric(l.Next()) {
+			l.Backup()
+			break
+		}
+	}
+	l.Emit(ItemVar)
+	return InsideAction
+}
+
+func InsideAction(l *lex.Lexer) lex.StateFn {
+	if strings.HasPrefix(l.Input[l.Pos:], rightDelim) {
+		return RightDelim
+	}
+
+	switch r := l.Next(); {
+	case r == lex.EOF || lex.IsEndOfLine(r):
+		// if reach eof throw while still in action throw error
+		return l.Errorf("unclosed action")
+	case lex.IsSpace(r):
+		return Space
+	case lex.IsAlphaNumeric(r):
+		l.Backup()
+		return Variable
+	}
+
+	return InsideAction
+}
+
+// Space scans a run of space characters.
+// One space has already been seen.
+func Space(l *lex.Lexer) lex.StateFn {
+	for lex.IsSpace(l.Peek()) {
+		l.Next()
+	}
+	l.Emit(ItemSpace)
+	return InsideAction
+}
+
+// RightDelim scans the right delimiter, which is known to be present.
+func RightDelim(l *lex.Lexer) lex.StateFn {
+	l.Pos += lex.Pos(len(rightDelim))
+	l.Emit(ItemRightDelim)
+	return LexTextNode
+}
+
+const (
+	StateLeftDelimLen = iota
+)
+
+// LeftDelim scans the left delimiter, which is known to be present.
+func LeftDelim(l *lex.Lexer) lex.StateFn {
+	l.Pos += lex.Pos(len(leftDelim))
+	l.Emit(ItemLeftDelim)
+	return InsideAction
+}
+
+func LexTextNode(l *lex.Lexer) lex.StateFn {
+	for {
+		if strings.HasPrefix(l.Input[l.Pos:], leftDelim) {
+			if l.Pos > l.Start {
+				l.Emit(ItemText)
+			}
+			return LeftDelim
+		}
+		if l.Next() == lex.EOF {
+			break
+		}
+	}
+	// Correctly reached EOF.
+	if l.Pos > l.Start {
+		l.Emit(ItemText)
+	}
+	l.Emit(lex.ItemEOF)
+	return nil
+}
+
+var debugNames = map[lex.ItemType]string{
+	lex.ItemError:  "ERROR",
+	lex.ItemEOF:    "EOF",
+	ItemText:       "TEXT",
+	ItemVar:        "VAR",
+	ItemSpace:      "SPACE",
+	ItemLeftDelim:  "LEFT DELIM",
+	ItemRightDelim: "RIGHT DELIM",
+}
+
+func ParseTextNode(input string) db.Domly {
+
+	l := createLexer("LextTextNode", input, LexTextNode)
+	for item := range l.Items {
+		fmt.Printf("Type: %20s\t %q\n", debugNames[item.Typ], item.Val)
+		if item.Typ == lex.ItemEOF {
+			break
+		}
+	}
+	return db.Domly{}
+
+}
+
 func GenDomlyNode(domNode *html.Node) db.Domly {
 	data := db.Domly{}
+	var textNodeDomly db.Domly
 	if domNode.Type == html.ElementNode {
 		data = append(data, domNode.Data)
 		if len(domNode.Attr) != 0 { // != nil {
@@ -59,8 +189,8 @@ func GenDomlyNode(domNode *html.Node) db.Domly {
 			if c.Type == html.ElementNode {
 				data = append(data, GenDomlyNode(c))
 			} else if c.Type == html.TextNode {
-				_ = parse.Parse // for each token parse attribute values and text nodes
-				data = append(data, c.Data)
+				textNodeDomly = ParseTextNode(c.Data) // for each token parse attribute values and text node
+				data = append(data, textNodeDomly)
 			}
 		}
 	}
