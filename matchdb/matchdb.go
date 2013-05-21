7a3fdbb88b4caccb0d20d25f80c179cb48e9e414
tav <tav@espians.com>
2013-05-21 03:00:22 +0100

    matchdb: rename source files to be more consistent.

--- matchdb/matchdb.go
+++ matchdb/matchdb.go
@@ -1,155 +1,156 @@
-// Public Domain (-) 2013 The Espra Authors.
+// Public Domain (-) 2012-2013 The Espra Authors.
 // See the Espra UNLICENSE file for details.
 
 package main
 
 import (
-	"encoding/binary"
-	"encoding/hex"
-	"github.com/dchest/siphash"
-	"github.com/tav/golly/aws"
+	"bufio"
+	"github.com/tav/golly/log"
+	"github.com/tav/golly/optparse"
 	"github.com/tav/golly/runtime"
 	"net"
+	"net/http"
+	"os"
+	"sync"
 	"time"
 )
 
-const (
-	routeMask = 0xffff
-	routeSize = 1 << 16
-)
-
-var (
-	cmdDelete = []byte{'D'}
-	cmdGet    = []byte{'G'}
-	cmdPing   = []byte{'P'}
-	cmdQuery  = []byte{'Q'}
-	cmdRenew  = []byte{'R'}
-	cmdSet    = []byte{'S'}
-)
-
-type Client struct {
-	free *Client
+type Entry struct {
+	id      string
+	listing []byte
 }
 
-type Node struct {
-	Addr  string
-	SeqID uint64
+type DB struct {
+	alloc       int64
+	limit       int64
+	limitNotify int64
+	id          []byte
+	master      MasterController
+	mutex       sync.Mutex
+	timeout     time.Duration
+	seen        map[string]int64
+	table       map[string]Entry
 }
 
-type MasterController interface {
-	IsSelf() bool
-	Run([]byte)
-}
-
-type Master struct {
-	addr         string
-	id           []byte
-	isSelf       bool
-	awsAccess    string
-	awsSecret    string
-	dynamoRegion *aws.Region
-	dynamoTable  string
-	httpClient   *http.Client
-	mutex        sync.Mutex
-	seen         map[string]int64
-	selfID       []byte
-	seqBytes     []byte
-	seqID        uint64
-	routes       [routeSize][]string
-}
-
-func (m *Master) IsDistributed() bool {
-	return true
-}
-
-func (m *Master) IsSelf() bool {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-	return m.isSelf
-}
-
-func (m *Master) Run(id []byte) {
-	m.selfID = id
-	for {
-
-	}
-}
-
-type LocalMaster struct {
-	addr     string
-	mutex    sync.Mutex
-	seqID    uint64
-	seqBytes []byte
-}
-
-func (m *LocalMaster) GetSeq() []byte {
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-	return m.seqBytes
-}
-
-func (m *LocalMaster) IsSelf() bool {
-	return true
-}
-
-func (m *LocalMaster) Run(id []byte) {
-}
-
-func NewMaster(singleNode, awsAccess, awsSecret, dynamoRegion, dynamoTable string, masterTimeout time.Duration) MasterController {
-	if awsAcess != "" || awsSecret != "" || dynamoTable != "" {
-		if awsAcess == "" {
-			runtime.Error("Distributed options set, but aws-access-key hasn't been specified")
-		}
-		if awsSecret == "" {
-			runtime.Error("Distributed options set, but aws-secret-key hasn't been specified")
-		}
-		if dynamoTable == "" {
-			runtime.Error("Distributed options set, but master-table hasn't been specified")
-		}
-		region, ok := aws.Regions[dynamoRegion]
-		if !ok {
-			runtime.Error("AWS Region %q not known", dynamoRegion)
-		}
-		return &Master{}
-	}
-	return &LocalMaster{addr: singleNode}
-}
-
-func NewClient(master MasterController) {
-}
-
-func encodeInt(v int64) {
+func (db *DB) Get() {
 	return
 }
 
-func getAddrFromID(id []byte) string {
-	return fmt.Sprintf("%s:%d", net.IP(id[:4]), binary.LittleEndian.Uint16(id[4:6]))
+func (db *DB) Ping() {
+	return
 }
 
-func getNodeID(host string, port int) []byte {
-	ip := net.ParseIP(host).To4()
-	if ip == nil || len(ip) != 4 {
-		runtime.Error("Could not parse host %q into a 32-bit IPv4 address", host)
-	}
+func (db *DB) Set() {
+	return
+}
+
+func (db *DB) Handle(conn net.Conn) {
+
 	id := make([]byte, 14)
-	copy(id[:4], ip)
-	binary.LittleEndian.PutUint16(id[4:6], uint16(port))
-	binary.LittleEndian.PutUint64(id[6:], uint64(time.Now().UnixNano()<<1))
-	return id
+
+	n, err := conn.Read(id)
+	if err != nil || n != 14 {
+		conn.Close()
+		return
+	}
+
+	addr := getAddrFromID(id)
+	r := bufio.NewReaderSize(conn, 1024)
+	w := bufio.NewWriterSize(conn, 1024)
+
+	defer conn.Close()
+
+	for {
+
+	}
+
 }
 
-func getSlot(v []byte) uint64 {
-	return siphash.Hash(hashKey0, hashKey1, v) & routeMask
+func (db *DB) Run(host string, port int, limit int64, master MasterController) error {
+
+	if host == "" {
+		host = runtime.GetIP()
+	}
+
+	db.id = getNodeID(host, port)
+	db.limit = limit << 10
+	db.limitNotify = (db.limit * 2) / 3
+
+	addr, listener := runtime.GetAddrListener(host, port)
+
+	defer listener.Close()
+	go master.Run(db.id)
+
+	log.Info("MatchDB running on %s", addr)
+
+	delay := 1 * time.Millisecond
+	maxDelay := 1 * time.Second
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				if delay == 0 {
+					delay = 5 * time.Millisecond
+				} else {
+					delay *= 2
+				}
+				if delay > maxDelay {
+					delay = maxDelay
+				}
+				log.Error("Accept error: %v; retrying in %v", err, delay)
+				time.Sleep(delay)
+				continue
+			}
+			log.Error("Accept error: %v", err)
+			return err
+		}
+		delay = 0
+		go db.Handle(conn)
+	}
+
 }
 
-func initHashKey(s string) {
-	if len(s) != 32 {
-		runtime.Error("The hash-key value %q is not a 32-byte string.", s)
-	}
-	k, err := hex.DecodeString(s)
-	if err != nil {
-		runtime.Error("The hash-key value %q is not a hex-encoded string: %s", s, err)
-	}
-	hashKey0 = binary.LittleEndian.Uint64(k[:8])
-	hashKey1 = binary.LittleEndian.Uint64(k[8:])
+func main() {
+
+	opts := optparse.Parser(
+		"Usage: matchdb <config.yaml> [options]\n",
+		"matchdb 0.0.1")
+
+	host := opts.StringConfig("host", "",
+		"the host to bind matchdb to")
+
+	port := opts.IntConfig("port", 8090,
+		"the port to bind matchdb to [8090]")
+
+	allocLimit := opts.IntConfig("alloc-limit", 5000000,
+		"maximum allocation limit in kilobytes [5000000]")
+
+	hashKey := opts.StringConfig("hash-key", "",
+		"16-byte hash key encoded as a 32-byte hex string")
+
+	awsAccessKey := opts.StringConfig("aws-access-key", "",
+		"the AWS Access Key for DynamoDB")
+
+	awsSecretKey := opts.StringConfig("aws-secret-key", "",
+		"the AWS Secret Key for DynamoDB")
+
+	awsRegion := opts.StringConfig("aws-region", "us-east-1",
+		"the AWS Region for DynamoDB [us-east-1]")
+
+	masterTable := opts.StringConfig("master-table", "",
+		"the DynamoDB table for the master lock")
+
+	masterTimeout := opts.IntConfig("master-timeout", 6000,
+		"timeout in milliseconds for the master lock [6000]")
+
+	os.Args[0] = "matchdb"
+	runtime.DefaultOpts("matchdb", opts, os.Args)
+
+	initHashKey(*hashKey)
+	master := NewMaster("", *awsAccessKey, *awsSecretKey, *awsRegion, *masterTable, time.Duration(*masterTimeout)*time.Millisecond)
+
+	db := &DB{}
+	db.Run(*host, *port, master, int64(*allocLimit))
+
 }
