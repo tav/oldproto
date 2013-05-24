0ea54e0ad07820e8da203162d03c3f31edc952ef
tav <tav@espians.com>
2013-05-24 02:37:30 +0100

    espra/kind: map the kind identifiers to more memorable constants.

---
+++ src/espra/kind/kind.go
@@ -0,0 +1,23 @@
+// Public Domain (-) 2013 The Espra Authors.
+// See the Espra UNLICENSE file for details.
+
+package kind
+
+// These constants define the kind identifiers for use in
+// the App Engine datastore.
+const (
+	AccessToken     = "AT"
+	Account         = "A"
+	AccountLogin    = "AL"
+	ClientLog       = "CL"
+	ClientToken     = "CT"
+	EmailAccount    = "EA"
+	GithubAccount   = "GA"
+	Index           = "N"
+	Item            = "I"
+	Pointer         = "P"
+	Space           = "S"
+	User            = "U"
+	UserIndex       = "UI"
+	UsernameAccount = "UA"
+)
